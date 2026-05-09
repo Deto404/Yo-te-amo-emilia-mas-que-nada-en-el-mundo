48abdd708626082fc99756ed87a2007de428ed7f
Alex <idkcoffee0.o@gmail.com>
2026-05-09 13:01:34 -0500

    Update script.js

--- I love you/I love you/Js/script.js
+++ I love you/I love you/Js/script.js
@@ -144,8 +144,8 @@ function showCountdown() {
   const container = document.getElementById('countdown');
   let startParam = getURLParam('start');
   let eventParam = getURLParam('event');
-  let startDate = startParam ? new Date(startParam + 'T00:00:00') : new Date('2025-11-25T00:00:00');
-  let eventDate = eventParam ? new Date(eventParam + 'T00:00:00') : new Date('2026-11-25T00:00:00');
+  let startDate = startParam ? new Date(startParam + 'T00:00:00') : new Date('2026-04-09T00:00:00');
+  let eventDate = eventParam ? new Date(eventParam + 'T00:00:00') : new Date('2027-04-09T00:00:00');
 
   function update() {
     const now = new Date();
